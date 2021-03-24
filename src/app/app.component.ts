c845f577c874718c8bfc557fd38810ec4ce8a676
grady <grady0902@gmail.com>
2021-03-24 20:11:15 +0800

    Updates Angular Material to v9

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import NP from 'number-precision';
 import { setTheme } from 'ngx-bootstrap/utils';
 import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 import { ClassInfo, RankInfo, RankType, ScoreType, SelectionObj, SemsScoreInfo, StudentInfo, SubjectOrDomainInfo, YearSemster } from './vo';
-import { MatGridTileHeaderCssMatStyler } from '@angular/material';
+import { MatGridTileHeaderCssMatStyler } from '@angular/material/grid-list';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
